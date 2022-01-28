66381b992e01ddda028c69bb32d88499c79148e0
Akihito <linkin_087@yahoo.co.jp>
2022-01-29 03:30:42 +0900

    random.choicesを使って簡略化

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import random, string
 
 number_of_digits = 12
 
-def gen_password(number_of_digits: int):
+def gen_password(number_of_digits: int) -> str:
     l, u, d, p = divide_area(number_of_digits)
-    print(l, u, d, p)
-    lower_text = [pick_one(string.ascii_lowercase) for i in range(l)]
-    upper_text = [pick_one(string.ascii_uppercase) for i in range(u)]
-    digit = [pick_one(string.digits) for i in range(d)]
-    punctuation = [pick_one('_-') for i in range(p)]
+    lower_text = random.choices(string.ascii_lowercase,k=l)
+    upper_text = random.choices(string.ascii_uppercase, k=u)
+    digit = random.choices(string.digits, k=d)
+    punctuation = random.choices('_-', k=2)
+
     word_list = lower_text + upper_text + digit + punctuation
     random.shuffle(word_list)
 
@@ -16,21 +16,18 @@ def gen_password(number_of_digits: int):
     print(password)
     return password
 
-def pick_one(strings: str) -> str:
-    return strings[random.randint(0, len(strings)-1)]
 
 def divide_area(num: int) -> int:
-    l = num//2
-    b = l // 2
-    c = b // 2
-    d = num - (l+b+c)
+    l = num // 2
+    u = l // 2
+    d = u // 2
+    p = num - (l+u+d)
+
     _ = random.randint(0,2)
-    if _ == 0:
-        u, d, p = b, c, d
-    elif _ == 1:
-        u, d, p = c, d, b
-    else:
-        u, d, p = d, b, c
+    if _ == 1:
+        u, d, p = d, p, u
+    elif _ == 2:
+        u, d, p = p, u, d
     return (l,u,d,p)
 
 
